c31a54f5c439cefe480e456028c3fbe6d0097e52
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-08 17:47:48 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  *main - entry point
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
