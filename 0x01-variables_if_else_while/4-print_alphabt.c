439e533892087f8482ac5212666f2fca15184346
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-11 10:29:42 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@ int main(void)
 {
 char c;
 
-for (c = 'a'; c <= 'z'; ++c)
+for (c = 'a' ; c <= 'z'; ++c)
 {
 if (c != 'e' && c != 'q')
 putchar(c);
 }
  
-putchar ('\n');
+putchar('\n');
 
 return (0);
 }
