a3c7efabc52e79ce95f6ec3ca6584e8bd314c610
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-12 20:36:39 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: prints _putchar using putchar prototype
  *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
 	char str[] = "_putchar";
         int ch;
 
