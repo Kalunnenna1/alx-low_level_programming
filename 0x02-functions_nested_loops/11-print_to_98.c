973184f88124aa3164d179d213cf5fb99ae66c4c
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-13 16:34:54 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,13 +7,13 @@
  *               space and number will be 
  *               in printed order
  *
- *@n: input
+ * @n: input
  */
 
 void print_to_98(int n)
 {
 	int count;
-
+	
 	if (n > 98)
 		for (count = n; count > 98; --count)
 			printf("%d, ", count);
