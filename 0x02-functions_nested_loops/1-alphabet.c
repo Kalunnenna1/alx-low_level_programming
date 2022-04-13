1cf2dc8134f97ffdf7193345150c980aa510ca84
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-13 01:32:59 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print 
- *                  the alphabet a - z
+ * print_alphabet -print alphabet lowercase
  *
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-int ch;
-	
-for (ch = 'a'; ch <= 'z'; ++ch)
-_putchar(ch);
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
 
 _putchar('\n');
+
 }
