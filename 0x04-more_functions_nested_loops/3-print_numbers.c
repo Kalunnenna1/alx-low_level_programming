c72ad92ce0b57a109051e90f1a693293c0913bb5
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-15 12:38:40 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * prints numbers from - print 0 - 9 with a new line
+ *                       using _putchar twice 
+ *
+ * Return: Return: Always 0 (Success)
+ */
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+
+}
+
+
