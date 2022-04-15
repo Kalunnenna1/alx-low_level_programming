c3db317fcc32f7c71db871a670a4a5542543c4b2
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-15 16:31:27 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,12 +23,12 @@ int main(void)
 		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
 		else
-			printf("%d", num)
+			printf("%d", num);
 
-				if (num != 100)
-					printf(" ");
-				else
-					printf("\n");
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 
 	return (0);
