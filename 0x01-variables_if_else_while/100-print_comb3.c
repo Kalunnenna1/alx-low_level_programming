86ad91ae5fb330b4b3a7993bb6e96fe524e8abb0
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-04-16 22:39:34 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits
- *         
- * Return: Always 0 (Success/completed)
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *              combinations of two digits.
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int digit1, digit2;
+	int digit1 = 0, digit2;
 
-for (digit1 = 0; digit1 < 9; digit1++)
-{
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-{
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
 
-if (digit1 == 8 && digit2 == 9)
-continue;
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++digit2;
+		}
+		++digit1;
+	}
+	putchar('\n');
 
-
-putchar(',');
-putchar(' ');
+	return (0);
 }
-}
-
-
-putchar('\n');
-
-
-return (0);
-}
-
-
 
