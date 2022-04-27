f1c93f3af9190a62dc4118347567569eb8c37d92
Kalunnenna1 <nnennakalualie.com>
2022-04-26 17:06:20 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,18 +8,10 @@ include "main.h"
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
-	unsigned int a;
+	unsigned int i;
 
-	a = 0;
-	while (i < n)  /*Delacring WHILE*/
-	{
-		*(dest + i) = *(src + i); /*add 1 position dest and src*/
-		i++;
-
-		} /*END WHILE*/
-
-
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
