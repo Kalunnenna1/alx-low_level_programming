ff25624b87738d0a2aa322fb38eae37a68f1377d
Kalunnenna1 <nnennakalualie.com>
2022-04-29 10:05:30 -0700

    recursion task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * sqrt_m - main - check the code
+ * @m: number
+ * @n: number of power
+ * Return: Always 0.
+ */
+
+int sqrt_m(int m, int n)
+{
+	if (n * n == m)
+	{
+		return (m);
+	}
+	else if (n * n > m)
+	{
+		return (-1);
+	}
+	return (sqrt_m(m, n + 1));
+}
+
+/**
+ * _sqrt_recursion - main - check the code
+ * @n: number
+ *
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_m(n, 0));
+}
