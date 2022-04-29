ae66d441a66ab071b447d9261b368d6da00c7fe8
Kalunnenna1 <nnennakalualie.com>
2022-04-29 09:24:09 -0700

    Recursion task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - main - a functions that returna value
+ * @x: interger
+ * @y: integer\
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0) /*Base condition*/
+	{
+		return (-1);
+	}
+
+	else if (y != 0)
+		return (x* _pow_recursion(x, y - 1)); /* Recursive call*/
+
+	else
+		return (1);
+}
