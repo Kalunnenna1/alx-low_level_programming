74357534af6019ddcc09002d0e3396387ee02385
Kalunnenna1 <nnennakalualie.com>
2022-05-03 06:29:01 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Prints the number of arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
 
 	return (0);
 }
