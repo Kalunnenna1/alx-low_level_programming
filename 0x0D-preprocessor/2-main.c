3ca8988db498126fb0c4538e4ef2c95b236b3873
Kalunnenna1 <nnennakalualie.com>
2022-05-11 13:48:04 -0700

    Task 2 for Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file 
+ * compiled that will be followed by a new line 
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+	printf("%\n", _FILE_);
+	return (0);
+}
