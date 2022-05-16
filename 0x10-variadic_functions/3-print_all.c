74a876fce572f3c26ba2703499c2c88c09f4c525
Kalunnenna1 <nnennakalualie.com>
2022-05-16 14:27:19 -0700

    variadic functions task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 void print_char(va_list arg);
 void print_int(va_list arg);
@@ -6,11 +8,6 @@ void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
-/**
- * print_char -print a char.
- * @arg: type of argument passing to the character
- * to be printed
- */
 /**
  * print_char - Prints a char.
  * @arg: A list of arguments pointing to
@@ -114,4 +111,3 @@ void print_all(const char * const format, ...)
 
 	va_end(args);
 }
-
