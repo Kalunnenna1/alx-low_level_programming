ddcecba8bed48932008162e4cb17168c3970c530
Kalunnenna1 <nnennakalualie.com>
2022-05-25 08:15:29 -0700

    advance task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void first(void) __attribute__((construction));
+void first(void) __attribute__ ((constructor));
 
 /**
  * first - prints a sentence before the main
- *         function is executed
+ * function is executed
  */
 void first(void)
 {
